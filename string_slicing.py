b1552d77b31334b4e3d67441fbac1135e094b766
Umar <attiqumarattiq@gmail.com>
2026-08-02 16:37:57 +0500

    Slicing with skip value

--- string_slicing.py
+++ string_slicing.py
@@ -1,5 +1,13 @@
 
-Name = "Umar Attiq"
+name = "Amazing"
 
-s = Name[0:7]           # Last index is not included, First is included
-print(s)
+print(name[1:6:2])
+
+# Slicing with skip value
+# Starts at index 1 ===== 'm'
+# Jumps forward 1 + 2 = 3, index 3 is 'z'
+# Jumps forward 3 + 2 = 5, index 5 is 'n'
+# Next stop 5 + 2 = 7, which is beyond the stop index 6, so python halts.
+
+##### Output is "mzn"
+
