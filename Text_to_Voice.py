f729dc5b1d1e5134a1e667c8365233e8c2ab7db8
Umar <attiqumarattiq@gmail.com>
2026-08-02 15:56:09 +0500

    Short form of code

--- Text_to_Voice.py
+++ Text_to_Voice.py
@@ -1,14 +1,4 @@
 import pyttsx3
-
-# Re-initialize the engine
 engine = pyttsx3.init()
-
-# Stop any previous hanging processes
-engine.stop()
-
-# Force speed rate adjustment to trigger SAPI5 reload
-engine.setProperty('rate', 175)
-
-# Speak
-engine.say("Sound check Sound check !! Hello Hello ")
+engine.say("Hy, how are u??")
 engine.runAndWait()
