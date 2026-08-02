546184614c79cd8f69dd0f6f410fbff74d961835
Umar <attiqumarattiq@gmail.com>
2026-08-02 16:08:27 +0500

    Type casting

---
+++ type()_Function_and_Typecasting.py
@@ -0,0 +1,10 @@
+
+a = 31
+type(a)    # class < int >
+
+b = "Only you" 
+type(b)    # class < string >
+
+str(31)         # Integer to string conversion
+int("32")      # String to integer conversion
+float(32)       # Integer to float conversion
