dd88a047adb964fff535064d88ec285ff630502a
Umar <attiqumarattiq@gmail.com>
2026-08-02 20:37:25 +0500

    More functions of stringgit add .

---
+++ String_functions2.py
@@ -0,0 +1,26 @@
+
+#===================================================
+
+# find(), returns the index of finding word
+text = "Hello, welcome to Python Programming"
+            # Returns starting index
+print(text.find("welcome"))    # Output: 7
+            # Gives -1, beacause this is case sensitive
+print(text.find("Welcome"))
+            # Gives -1, beacuse substring not found
+print(text.find("Java"))
+
+#====================================================
+
+# replace(Old word, New word), replaces old word with the new word
+str = "Jobless man"
+print(str.replace("Jobless","Helping"))
+print(str)
+
+#=========================================================
+
+# \n,  uses for shifting to next line
+
+
+
+ 
