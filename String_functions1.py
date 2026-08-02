deff60324765d98e8f5046fde11a6908549f7194
Umar <attiqumarattiq@gmail.com>
2026-08-02 19:02:37 +0500

    Modifies the code

--- String_functions1.py
+++ String_functions1.py
@@ -1,18 +1,19 @@
-
+#==============================================================================
  # len() returns the length of function
 name = "Your name"
 print(len(name))
-
+#==============================================================================
 # endswith() checks if a string ends with given text, give output TRUE or FALSE
 name = "Umar"
 print(name.endswith("ar"))
-
+#==============================================================================
 # count(), count the occurance of a character
 name = "Hy, how are u doing !! BRo All ok "
 count = name.count("o")
 print(count)
-
+#==============================================================================
 # capitaize(), capitalizes the first character
 name = "waqas"
 print(name.capitalize())
+#=============================================================================
 
