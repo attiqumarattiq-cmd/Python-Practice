8de0152bdde7a683e2b8d8e74595d9a38294f3fb
Umar <attiqumarattiq@gmail.com>
2026-08-03 20:04:44 +0500

    List Functions Helpful

---
+++ List_Functions_sorting_reversing_appending_etc.py
@@ -0,0 +1,28 @@
+
+List = [1,56,23,5,12,9,7,99]
+
+#=========================================================
+# sort(): Updates the list in regular order
+List.sort()
+print(List)
+
+#=========================================================
+# reverse(): Updates the list in reverse order of numbering
+List.reverse()
+print(List)
+
+#============================================================
+# append(8): Adds the number at the end of the list
+List.append(8)
+print(List) 
+
+#============================================================
+# insert(3,8): This will add 8 at index 3
+List.insert(3,8)
+print(List)
+
+#==============================================================
+# pop(2): It will delete element at index 2, and return its value
+List.pop(2)
+print(List)
+
