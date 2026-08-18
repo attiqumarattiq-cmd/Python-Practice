61f633d68df38fe4a815e2fb9a9b7261ac399cae
Umar <attiqumarattiq@gmail.com>
2026-08-18 11:24:57 +0500

    Adding new files and concept of python like fromating

---
+++ tempCodeRunnerFile.python
@@ -0,0 +1,12 @@
+
+
+# Formatting strings
+
+value = 4
+print("Value is", value)
+
+# Format() Function
+
+stock_ticker = 'AAPL'
+price = 226.42
+print("We are interested in {x} which is currently trading at {y}".format(x=stock_ticker, y=price))
---
+++ type_error_of_string_replacing_word.py
@@ -0,0 +1,9 @@
+
+# Strings are immutable - This means once we have created a string we cannot
+#     change it. Consider the following example.
+month_name = 'Fenuary'
+
+month_name[0] = 'J'
+
+print(month_name)         # This gives type error
+ 
