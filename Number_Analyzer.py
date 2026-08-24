dedfd7179f6059b53e562752ada80d355b31bd87
Umar <attiqumarattiq@gmail.com>
2026-08-24 22:31:10 +0500

    Added another code of python

--- Number_Analyzer.py
+++ Number_Analyzer.py
@@ -1,2 +1,19 @@
 
-number = input("Enter a number: ")
+number = int(input("Enter a number: "))
+
+if number >= 0:
+    print("Number is Positive")
+elif number < 0:
+    print("Number is Negarive")
+    
+if number % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+    
+square = number * number
+cube = number * number * number
+
+print("Square:", square)
+print("Cube:", cube)
+print(type(number))
