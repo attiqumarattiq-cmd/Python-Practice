f0da42412baa9a9af820e2679ea093cd9f666679
Umar <attiqumarattiq@gmail.com>
2026-08-27 20:15:05 +0500

    Types of range function

---
+++ Range()_function_types_of_using_it.py
@@ -0,0 +1,29 @@
+
+# Type 1
+for i in range(5):
+    print(i)
+    
+print("------")
+
+# Type 2
+for i in range(7,20):
+    print(i)
+
+print("------")
+
+# Type 3
+for i in range(5,15,2):
+    print(i)
+    
+print("------")
+
+# Type 3 (Reverse Counting)
+for i in range(10, 0, -1):
+    print(i)
+    
+print("------")
+
+
+
+    
+    
