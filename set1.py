bae2e21c0f1a21e8b37126ac17c6337b62c3d32c
Umar <attiqumarattiq@gmail.com>
2026-09-10 22:30:00 +0500

    Just added some space in file......

--- set1.py
+++ set1.py
@@ -2,4 +2,4 @@ set1 = {1, 2, 3, 4, 5}
 set2 = {2, 3, 4}
 
 print(set2.issubset(set1))    # is set2 a subset of set1?
-print(set1.issuperset(set2))  # is set1 a superset of set2?
+print(set1.issuperset(set2))  # is set1 a superset of set2? 
